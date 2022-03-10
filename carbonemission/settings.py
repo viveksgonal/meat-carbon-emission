eb26bd8c8c7c7f1c6dcf49ac5ff81ac984809733
Divesh Watwani <60432327+diveshwatwani@users.noreply.github.com>
2022-03-10 17:06:03 +0530

    v9.5

--- carbonemission/settings.py
+++ carbonemission/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-8!nm58fux@9*i^qyipg6o!8zur8mv63zd9bp9yxljrpdm&$m@a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('DJANGO_HOST', 'localhost')]
+#ALLOWED_HOSTS = [os.environ.get('DJANGO_HOST', 'localhost')]
+ALLOWED_HOSTS = ['https://meat-carbon-emission.azurewebsites.net/','localhost','127.0.0.1:8000']
 
 
 # Application definition
